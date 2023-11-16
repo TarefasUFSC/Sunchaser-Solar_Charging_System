62b3108ed00d84ef0cc6ebf819602f910996aded
CodeWracker <dev.rodrigofs@gmail.com>
2023-11-16 12:01:20 -0300

    base para o codigo final

---
+++ Programas ESP/ProgramaFinal/ProgramaFinal.ino
@@ -0,0 +1,46 @@
+
+
+class Sensors
+{
+};
+
+class BatteryController
+{
+};
+
+class Comunicator
+{
+};
+
+class FileSystem
+{
+};
+
+class TimerInterrupt
+{
+};
+
+
+// Cria as instancias globais das classes
+
+void setup()
+{
+    // faz as configurações iniciais de cada objeto, dos attach dos interrupts
+
+    // verifica o tempo da rede
+}
+
+void loop()
+{
+
+
+   // chama a função de verificar flags do comunicador
+
+
+    // if( not comunidaor.isServer)
+        // verifica o counter do TimerInterrupt e faz as chamadas
+        // timer_interruption(fileSystem, Sensors, comunicator) // -> faz as verificações de tempo e levanta as flags de processos
+
+        // chama a função de controle da bateria
+        // battery_loop(sensors)
+}
