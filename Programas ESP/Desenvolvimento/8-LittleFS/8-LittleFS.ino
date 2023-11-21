0cd3f20d8a5a055a853730b62c14788883543af0
ManoellaR <manu.rockemba@gmail.com>
2023-11-21 10:58:12 -0300

    Terminei a implementação das classes

--- Programas ESP/Desenvolvimento/8-LittleFS/8-LittleFS.ino
+++ Programas ESP/Desenvolvimento/8-LittleFS/8-LittleFS.ino
@@ -1,17 +1,14 @@
 #include "TimerInterrupt.h"
 #include "SaveToFlash.h"
 
-TimerInterrupt timerInterrupt = TimerInterrupt();
+SaveToFlash fileSystem;
+TimerInterrupt timerInterrupt(fileSystem);
 
 void setup(){
     Serial.begin(115200);
     timerInterrupt.timer_init();
-    mountLittleFS();
-    
-    listDir(LittleFS, "/", 3);
+    fileSystem.mountLittleFS();
 }
-
-//int count = 0;
 
 void loop(){
     timerInterrupt.timer_interruption();
